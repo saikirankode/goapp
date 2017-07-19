5e0378a5cac36609ab44ebf4e7d6e513d934a73b
saikirankode <saikirankode@gmail.com>
2017-07-19 18:01:39 -0400

    corrections

--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
-func main(){
+
+func main() {
 	http.HandleFunc("/", handle)
 	http.HandleFunc("/_ah/health", healthCheckHandler)
 	log.Print("Listening on port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handle(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path != "/"{
-		http.NotFound(w,r)
+func handle(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
 		return
 	}
 	fmt.Fprint(w, "Hello, World!")
 }
-func healthCheckHandler(w http.ResponseWriter, r *http.Request){
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "ok")
 }
